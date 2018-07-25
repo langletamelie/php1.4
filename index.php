94a664a89e35b4fba7ffdcb2dd0250cde18c3056
langletamelie <amelie.langlet@hotmail.com>
2018-07-25 19:03:05 +0200

    php1.4

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP 1.4</title>
+  </head>
+  <body>
+<p>
+<?php
+$string = 'ceci est une phrase';
+$int = 17;
+$float = 65.96;
+$toujours_une_phrase = true;
+echo $string . ' '. $int . ' ' . $float . ' ' . true;
+ ?>
+</p>
+  </body>
+</html>
